aa7135dec43172ac8124f263fa17564b16ee47a8
KotaKato <katokota1102@icloud.com>
2019-04-13 21:23:08 +0900

    ボタンを作った。

--- src/calc/calc/ViewController.swift
+++ src/calc/calc/ViewController.swift
@@ -22,6 +22,43 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func ACButton(_ sender: Any) {
+    }
+    @IBAction func CButton(_ sender: Any) {
+    }
+    @IBAction func PlusOrMinusButton(_ sender: Any) {
+    }
+    @IBAction func PlusButton(_ sender: Any) {
+    }
+    @IBAction func MinusButton(_ sender: Any) {
+    }
+    @IBAction func AsteliskButton(_ sender: Any) {
+    }
+    @IBAction func SlashButton(_ sender: Any) {
+    }
+    @IBAction func EqualButton(_ sender: Any) {
+    }
+    @IBAction func DotButton(_ sender: Any) {
+    }
+    @IBAction func OneButton(_ sender: Any) {
+    }
+    @IBAction func TwoButton(_ sender: Any) {
+    }
+    @IBAction func ThreeButton(_ sender: Any) {
+    }
+    @IBAction func FourButton(_ sender: Any) {
+    }
+    @IBAction func FiveButton(_ sender: Any) {
+    }
+    @IBAction func SixButton(_ sender: Any) {
+    }
+    @IBAction func SevenButton(_ sender: Any) {
+    }
+    @IBAction func EightButton(_ sender: Any) {
+    }
+    @IBAction func NineButton(_ sender: Any) {
+    }
+    @IBAction func ZeroButton(_ sender: Any) {
+    }
 }
 
